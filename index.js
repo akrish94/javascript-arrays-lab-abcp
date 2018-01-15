1744d92be0a2651a4a171176aaff343bd7925d1a
Adithi Krishnamoorthy <adithikrishnamoorthy@gmail.com>
2018-01-15 21:27:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,16 +3,10 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
-  kittens.pop(name)
+  kittens.push(name)
 }
 
-  describe('destructivelyAppendKitten(name)', function() {
-    it('appends a kitten to the end of the kittens array', function() {
-      destructivelyAppendKitten('Ralph')
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
-    })
-  })
-
+  
   describe('destructivelyPrependKitten(name)', function() {
     it('prepends a kitten to the beginning of the kittens array', function() {
       destructivelyPrependKitten("Bob")
