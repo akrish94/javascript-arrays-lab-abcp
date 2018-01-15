fd8fb66ccf09bd63870f6d947f3220370aae84e2
Adithi Krishnamoorthy <adithikrishnamoorthy@gmail.com>
2018-01-15 21:39:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,16 +33,14 @@ function prependKitten(name){
   
 
 function removeLastKitten(){
-  
+  return kittens.slice(1)
 }  
 
-  describe('removeLastKitten()', function() {
-    it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      expect(removeLastKitten()).toEqual(["Milo", "Otis"])
 
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
+function removeFirstKitten(){
+  return 
+}
+  
 
   describe('removeFirstKitten()', function() {
     it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
